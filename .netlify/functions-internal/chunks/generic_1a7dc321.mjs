6832055ef153e1687f8e78e841c1433dc1b599b0
Amer IdeaPad <amer.tricic@live.com>
2023-11-11 21:49:55 +0100

    update

--- .netlify/functions-internal/chunks/generic_1a7dc321.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_4eaa212e.mjs');
-
-export { page };
